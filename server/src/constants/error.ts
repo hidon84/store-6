3ea596334e849416d3091d38a6fcee1b5631ae67
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-14 19:41:19 +0900

    [#13]feat: auth service에 로그인 메서드 추가

--- server/src/constants/error.ts
+++ server/src/constants/error.ts
@@ -7,3 +7,14 @@ export const ERROR_UNKNOWN = {
   statusCode: 500,
   message: 'Unkown Error',
 };
+
+export const loginError = {
+  unable: {
+    statusCode: 500,
+    message: 'Unable to Login',
+  },
+  notFound: {
+    statusCode: 404,
+    message: 'User Not Found',
+  },
+};
--- server/src/service/auth.ts
+++ server/src/service/auth.ts
@@ -1,18 +1,52 @@
 import { Service, Inject } from 'typedi';
-import User from '@/entity/user';
+import LoginModel from '@/model/login';
+import * as hashHelper from '@/helper/hash';
+import * as jwtHelper from '@/helper/jwt';
+import * as authHelper from '@/helper/auth';
+import UserModel from '@/model/user';
+import ErrorResponse from '@/utils/errorResponse';
+import { loginError } from '@/constants/error';
 
 @Service()
 class AuthService {
-  private userEntity: User;
+  private loginModel: LoginModel;
 
-  constructor(@Inject('userEntity') userEntity: User) {
-    this.userEntity = userEntity;
+  private userModel: UserModel;
+
+  constructor(
+    @Inject('loginModel') loginModel: LoginModel,
+    @Inject('userModel') userModel: UserModel,
+  ) {
+    this.loginModel = loginModel;
+    this.userModel = userModel;
   }
 
-  async Signup() {
-    const userNumber = new Date().getTime();
-    this.userEntity.userId = `testId${userNumber}`;
-    this.userEntity.save();
+  async Login(id: string, password: string) {
+    try {
+      const login = await this.loginModel.findById(id);
+
+      if (!login) {
+        throw new ErrorResponse(loginError.notFound);
+      }
+
+      const user = await this.userModel.findUserByLoginIdx(login.idx);
+      const isValid = hashHelper.comparePassword(login.password, password);
+
+      if (isValid) {
+        const access = jwtHelper.generateAccessToken(user);
+        const refresh = jwtHelper.generateRefreshToken(user);
+
+        await authHelper.storeRefreshToken(refresh, login.idx);
+
+        return { access, refresh };
+      }
+      return {};
+    } catch (e) {
+      if (e instanceof ErrorResponse) {
+        throw e;
+      }
+      throw new ErrorResponse(loginError.unable);
+    }
   }
 }
 
