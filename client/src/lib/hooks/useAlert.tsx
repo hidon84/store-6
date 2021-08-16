2e7ca422d00c398719eefb4e1bbd791db28b773d
jonghopark95 <jong951005@gmail.com>
2021-08-17 07:58:08 +0900

    feat: Create useAlert hook
    Co-Authored-By: Inkyu Oh <gomjellie@gmail.com>

---
+++ client/src/lib/hooks/useAlert.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+/**
+ * Alert 모달을 호출하기 위해 사용하는 훅입니다.
+ * @example
+ *  const { alert } = useAlert();
+ *  ...
+ *  onClick={() => alert('아하하하하하하하')};
+ */
+const useAlert = () => {
+  let alertModalDOM: HTMLElement;
+
+  useEffect(() => {
+    alertModalDOM = document.querySelector('#portal > aside');
+  }, []);
+
+  const alert = (content: string) => {
+    alertModalDOM.classList.add('show');
+    alertModalDOM.querySelector('span').innerText = content;
+
+    setTimeout(() => {
+      alertModalDOM.classList.remove('show');
+    }, 1500);
+  };
+
+  return { alert };
+};
+
+export default useAlert;
