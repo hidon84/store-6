a548f90c670a748a50a328ab09c3b84df1a1db9a
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-16 22:03:38 +0900

    [#24]refactor: baseURL을 환경변수로 받도록 수정

--- client/src/lib/api/client.ts
+++ client/src/lib/api/client.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const client = axios.create();
 
-// client.defaults.baseURL = 'https://???';
+client.defaults.baseURL = process.env.API_URL ?? '/';
 
 export default client;
