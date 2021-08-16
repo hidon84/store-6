fafcdc70413d99517af12fe23617d1759b30672c
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-16 17:52:09 +0900

    [#29]doc: jsdoc을 사용해서 미들웨어 부분에 설명 추가

--- server/src/api/middlewares/isAuth.ts
+++ server/src/api/middlewares/isAuth.ts
@@ -2,6 +2,13 @@ import jwt from 'express-jwt';
 import { Request } from 'express';
 import config from '@/config';
 
+/**
+ * Header의 authorizatioin 필드에서 Token을 파싱해 반환합니다.
+ *
+ * @param req Express Request 객체
+ *
+ * @returns Header에서 파싱한 Token 문자열. 만약 없을 경우 null 반환
+ */
 const getTokenFromHeader = (req: Request) => {
   const { authorization } = req.headers;
   if (
@@ -13,6 +20,16 @@ const getTokenFromHeader = (req: Request) => {
   return null;
 };
 
+/**
+ * express-jwt 패키지를 사용한 토큰 검증 미들웨어입니다.
+ *
+ * 토큰을 파싱해서 req.token에 토큰 디코딩된 객체를 넣습니다.
+ *
+ * 이 미들웨어를 통과하게 되면 토큰이 있다는 것이 검증된 것이므로
+ * isAuth라는 네이밍을 사용했습니다.
+ *
+ * @error 만약 토큰이 없으면 Unauthorized 에러를 응답합니다.
+ */
 const isAuth = jwt({
   secret: config.jwt.secret,
   algorithms: [config.jwt.algorithm],
--- server/src/api/middlewares/isJwtToken.ts
+++ server/src/api/middlewares/isJwtToken.ts
@@ -11,5 +11,20 @@ const verifyTokenSub =
     return next(new ErrorResponse(commonError.unauthorized));
   };
 
+/**
+ * Token의 Subject를 검사해서 Refresh Token인지를 검증합니다.
+ *
+ * @error Refresh Token이 아닐 경우 unauthorized 에러를 응답합니다.
+ *
+ * @need isAuth 미들웨어를 이 미들웨어 이전에 사용해야 합니다.
+ */
 export const isRefreshToken = verifyTokenSub(Token.Refresh);
+
+/**
+ * Token의 Subject를 검사해서 Access Token인지를 검증합니다.
+ *
+ * @error Access Token이 아닐 경우 unauthorized 에러를 응답합니다.
+ *
+ * @need isAuth 미들웨어를 이 미들웨어 이전에 사용해야 합니다.
+ */
 export const isAccessToken = verifyTokenSub(Token.Access);
