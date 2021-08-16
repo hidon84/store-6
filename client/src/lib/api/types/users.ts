c9643a946bf09d2f6a0ead828d2764e4e2574cd1
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-16 21:59:18 +0900

    [#24]refactor: api user request/response 타입 리팩토링

--- client/src/lib/api/types/users.ts
+++ client/src/lib/api/types/users.ts
@@ -1,52 +1,26 @@
-import { ApiResponse, UpdateInfo } from './common';
+import { UpdateInfo } from './common';
 
-export interface UserInfo {
+export interface UsersPostRequestBody {
+  id: string;
+  password: string;
+  type?: string;
+  email?: string;
+  phone: string;
+  profile?: string;
+}
+export type UsersPostResponseBody = UpdateInfo;
+
+export interface UsersGetResponseBody extends UpdateInfo {
+  id: string;
+  email: string;
+  phone: string;
+  profile: string;
+}
+
+export interface UsersPutRequestBody {
+  password?: string;
   email?: string;
   phone?: string;
   profile?: string;
 }
-
-export interface User extends UserInfo, UpdateInfo {
-  id: string;
-  phone: string;
-}
-
-/// //////////////////////////////////////////////////////////
-
-export interface UsersPostRequestBody extends UserInfo {
-  id: string;
-  password: string;
-  phone: string;
-  type?: string;
-}
-export interface UsersPutRequestBody extends UserInfo {
-  password?: string;
-}
-export interface UsersGetResponseBody extends User {
-  createdAt: string;
-  updatedAt: string;
-}
-export interface UsersPostResponseBody extends UpdateInfo {
-  createdAt: string;
-  updatedAt: string;
-}
-export interface UsersPutResponseBody extends UpdateInfo {
-  createdAt: string;
-  updatedAt: string;
-}
-
-/// //////////////////////////////////////////////////////////
-
-export interface UserResponse extends ApiResponse, UpdateInfo {
-  createdAt: Date;
-  updatedAt: Date;
-}
-export interface UsersGetResponse extends UserResponse, User {
-  id: string;
-  email: string;
-  profile: string;
-  createdAt: Date;
-  updatedAt: Date;
-}
-export type UsersPostResponse = UserResponse;
-export type UsersPutResponse = UserResponse;
+export type UsersPutResponseBody = UpdateInfo;
