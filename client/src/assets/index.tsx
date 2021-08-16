35da41e18e3a50a5538ad003e7a047e22ce6786f
jonghopark95 <jong951005@gmail.com>
2021-08-16 17:54:17 +0900

    chore: define Header, Common SVG on assets
    Co-Authored-By: Inkyu Oh <gomjellie@gmail.com>

--- client/src/assets/index.tsx
+++ client/src/assets/index.tsx
@@ -11,7 +11,25 @@ import socialGoogleSVG from './social-google.svg';
 import underlineSVG from './underline.svg';
 import verticalLineSVG from './vertical-line.svg';
 
+const S3_PREFIX = 'https:/store-6-bucket.s3.ap-northeast-2.amazonaws.com';
+
+const LogoSVG = `${S3_PREFIX}/common/logo.svg`;
+const LineSVG = `${S3_PREFIX}/common/line.svg`;
+
+const DoodleUselessSVG = `${S3_PREFIX}/header/doodle-useless.svg`;
+const HeartSVG = `${S3_PREFIX}/header/heart.svg`;
+const MypageSVG = `${S3_PREFIX}/header/mypage.svg`;
+const CartSVG = `${S3_PREFIX}/header/shop.svg`;
+const BadgeSVG = `${S3_PREFIX}/header/badge.svg`;
+
 export {
+  LogoSVG,
+  DoodleUselessSVG,
+  HeartSVG,
+  MypageSVG,
+  CartSVG,
+  LineSVG,
+  BadgeSVG,
   buttonSVG,
   checkCircleSVG,
   checkSVG,
