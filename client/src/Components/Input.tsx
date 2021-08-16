bba152e637cbdf579b0f6c5d5eb0cdf6471f94e2
gomjellie <gomjellie@gmail.com>
2021-08-16 15:38:51 +0900

    [#15]chore: input width를 props로 받을 수 있게 변경
    Co-authored-by: jonghopark95 <jong951005@gmail.com>

--- client/src/Components/Input.tsx
+++ client/src/Components/Input.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 import { underlineSVG } from '~/assets/index';
 
-const Input = styled.input`
+/**
+ * width에 string으로 %를 넘겨주세요
+ * example) <Input width="100%" />
+ */
+const Input = styled.input<{ width?: string }>`
   font-size: 20px;
   margin-top: 16px;
-  width: 100%;
+  width: ${(props) => props.width || '100%'};
   padding-left: 16px;
   line-height: 25px;
   height: 34px;
