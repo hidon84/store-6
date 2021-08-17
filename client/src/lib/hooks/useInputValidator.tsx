86ec405503eb4941478ead4ee50e84b614765f7a
gomjellie <gomjellie@gmail.com>
2021-08-17 16:56:09 +0900

    [#17]feat: useInputValidator hook 생성

---
+++ client/src/lib/hooks/useInputValidator.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+function useInputValidator(
+  initialValue: string,
+  validator: (user_input: string) => string,
+) {
+  const [input, setInput] = useState(initialValue);
+  const [warning, setWarning] = useState('');
+
+  const handleInput = (user_input: string) => {
+    setInput(user_input);
+    setWarning(validator(user_input));
+  };
+  return [input, warning, handleInput] as const;
+}
+
+export default useInputValidator;
