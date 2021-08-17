46501ba1aac806fa346ba83605f1592852831bb7
qudgus21 <qudgus54321@naver.com>
2021-08-18 04:40:12 +0900

    [#31]feat: MyPage 페이지 추가
    Co-Authored-By: Yunseo Hwang <yunseo.dev@gmail.com>

--- client/src/Pages/MyPage/index.tsx
+++ client/src/Pages/MyPage/index.tsx
@@ -1,91 +1,119 @@
-import React, {
-  useState,
-  useRef,
-  RefObject,
-  useEffect,
-  useCallback,
-} from 'react';
-import Input from '~/components/Input';
+import React, { useState, useRef, RefObject, useEffect } from 'react';
 import Divider from '~/components/Divider';
-import PhoneInput from '~/components/PhoneInput';
-import { getMe } from '~/lib/api/users';
+import { getMe, putMe } from '~/lib/api/users';
 import { login } from '~/lib/api/auth';
-import { UsersGetResponseBody } from '~/lib/api/types/users';
-
+import {
+  UsersGetResponseBody,
+  UsersPutResponseBody,
+} from '~/lib/api/types/users';
+import {
+  REG_EMAIL,
+  WARNING_EMAIL,
+  REG_IMAGE,
+  REG_PHONE,
+} from '~/utils/validation';
+import { alert } from '~/utils/modal';
+import UserInfoInput from '~/components/UserInfoInput';
+import PhoneInput from '~/components/PhoneInput';
+import EmailInput from '~/components/EmailInput';
 import {
   StyledMyPage,
   Title,
   RowWrapper,
   RowTitle,
   PhotoWrapper,
-  Button,
   ImagePreview,
-  EmailWrapper,
   ImageDesc,
   ImageInput,
 } from './index.style';
+
+const message = {
+  EMAIL_UPDATE_SUCCESS: '이메일이 수정되었습니다.',
+  PHONE_UPDATE_SUCCESS: '연락처가 수정되었습니다.',
+  PHOTO_UPDATE_SUCCESS: '사진이 수정되었습니다.',
+  NOT_CORRECT_IMAGE: '올바른 이미지파일 형식이 아닙니다.',
+  EMAIL_UPDATE_FAIL: '이메일 수정에 실패했습니다.',
+  PHONE_UPDATE_FAIL: '연락처 수정에 실패했습니다.',
+};
 
 const MyPage: React.FC = () => {
   const [profile, setProfile] = useState<string>(
     'https://user-images.githubusercontent.com/47776356/129712816-13701b24-57cc-451e-93c6-ca7afe190af1.jpeg',
   );
-  const [email, setEmail] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string[]>(['', '', '']);
 
+  const [userInfo, setUserInfo] = useState<UsersGetResponseBody | null>(null);
   const imagePreviewRef: RefObject<HTMLLabelElement> = useRef();
 
-  const handleImageInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const reader = new FileReader();
-    const file = e.target.files[0];
-
-    if (file && file.type.match('image.*')) {
-      reader.readAsDataURL(e.target.files[0]);
+  const handleSubmitEmail = async (value: string) => {
+    const response = await putMe({ email: value });
+    if (response.statusCode === 200) {
+      setUserInfo({ ...userInfo, email: value });
+      alert(message.EMAIL_UPDATE_SUCCESS);
     }
+  };
 
+  const handleSubmitPhone = async (value: string) => {
+    const response = await putMe({ phone: value });
+    if (response.statusCode === 200) {
+      alert(message.PHONE_UPDATE_SUCCESS);
+      setUserInfo({ ...userInfo, phone: value });
+    }
+  };
+
+  const handleSubmitProfile = async (file, fileToString) => {
+    const response = await putMe({
+      img: file,
+    });
+
+    if (response.statusCode === 200) {
+      setProfile(fileToString);
+      alert(message.PHOTO_UPDATE_SUCCESS);
+    }
+  };
+
+  const handleImageInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files[0];
+    if (!file.type.match(REG_IMAGE)) {
+      e.target.value = '';
+      alert(message.NOT_CORRECT_IMAGE);
+      return;
+    }
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
     reader.onload = (event) => {
-      setProfile(event.target.result.toString());
+      handleSubmitProfile(file, event.target.result.toString());
     };
+  };
+
+  const emailValidator = (value: string) => {
+    return REG_EMAIL.test(value);
+  };
+  const phoneValidator = (value: string) => {
+    return REG_PHONE.test(value);
   };
 
   useEffect(() => {
     imagePreviewRef.current.style.backgroundImage = `url(${profile})`;
   }, [imagePreviewRef, profile]);
 
-  const handleOnChangePhoneNumber = useCallback(
-    (phone: string[]) => {
-      setPhoneNumber(phone);
-      console.log(phone);
-    },
-    [setPhoneNumber],
-  );
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
   useEffect(() => {
     const fetchUser = async () => {
-      const test = await login({
-        id: 'test',
-        password: 'hahahahoho',
-      });
+      // 로그인 기능이 아직 구현되어있지 않아, 테스트를 위해 로그인 로직을 추가해놓았습니다.
+      // const test = await login({
+      //   id: 'test',
+      //   password: 'hahahahoho',
+      // });
 
       const response = await getMe();
 
       if (response.statusCode === 200) {
         const user = response.data as UsersGetResponseBody;
 
-        if (user.email) {
-          setEmail(user.email);
-        }
-
-        if (user.phone) {
-          setPhoneNumber(user.phone.split('-'));
-        }
-
         if (user.profile) {
           setProfile(user.profile);
         }
+
+        setUserInfo(user);
       }
     };
     fetchUser();
@@ -115,31 +143,27 @@ const MyPage: React.FC = () => {
 
         <Divider width="630px" direction="horizontal" />
 
-        <RowWrapper>
-          <RowTitle>이메일</RowTitle>
-          <EmailWrapper>
-            <Input
-              type="text"
-              value={email}
-              onChange={handleEmailChange}
-              placeholder="이메일을 입력해 주세요."
-              width="370px"
-              style={{ fontSize: '14px' }}
-            />
-          </EmailWrapper>
-          <Button>수정</Button>
-        </RowWrapper>
+        <UserInfoInput
+          title="이메일"
+          showWarning={() => alert(message.EMAIL_UPDATE_FAIL)}
+          value={userInfo?.email}
+          placeholder={userInfo?.email}
+          onSubmit={handleSubmitEmail}
+          inputComponent={EmailInput}
+          validator={emailValidator}
+        />
 
         <Divider width="630px" direction="horizontal" />
 
-        <RowWrapper>
-          <RowTitle>연락처</RowTitle>
-          <PhoneInput
-            onChange={handleOnChangePhoneNumber}
-            value={phoneNumber}
-          />
-          <Button>수정</Button>
-        </RowWrapper>
+        <UserInfoInput
+          title="연락처"
+          showWarning={() => alert(message.PHONE_UPDATE_FAIL)}
+          value={userInfo?.phone}
+          placeholder={userInfo?.phone?.split('-').join(' ')}
+          onSubmit={handleSubmitPhone}
+          inputComponent={PhoneInput}
+          validator={phoneValidator}
+        />
       </div>
     </StyledMyPage>
   );
