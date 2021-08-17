ef5f681becf1b844220dd466ef76b8cf730b9f8f
qudgus21 <qudgus54321@naver.com>
2021-08-18 04:39:11 +0900

    [#31]feat: PhoneInput 컴포넌트 추가
    Co-Authored-By: Yunseo Hwang <yunseo.dev@gmail.com>

--- client/src/components/PhoneInput/index.tsx
+++ client/src/components/PhoneInput/index.tsx
@@ -1,39 +1,56 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { Fragment, useCallback, useMemo } from 'react';
 import Input from '~/components/Input';
 import Divider from '~/components/Divider';
 
 import { PhoneWrapper } from './index.style';
+import { REG_NOT_DIGITS } from '~/utils/validation';
 
 interface Props {
-  onChange?: (phoneNumber: string[]) => void;
-  value?: string[];
+  onChange?: (phoneNumber: string) => void;
+  value?: string;
+  disabled?: boolean;
+  placeholder?: string;
 }
 
 const phoneNumbersInfo = [
-  { key: 1, max: 3 },
-  { key: 2, max: 4 },
-  { key: 3, max: 4 },
+  { key: 1, max: 3, placeholder: '입력이' },
+  { key: 2, max: 4, placeholder: '필요' },
+  { key: 3, max: 4, placeholder: '합니다' },
 ];
 
-const PhoneInput: React.FC<Props> = ({ onChange, value }) => {
+const PhoneInput: React.FC<Props> = ({
+  onChange,
+  value,
+  disabled,
+  placeholder,
+}) => {
   const phoneValue = useMemo(() => {
     const result = phoneNumbersInfo.map(() => '');
-    if (!value) return result;
-    value.forEach((v, i) => {
+    value?.split('-').forEach((v, i) => {
       result[i] = v;
     });
     return result;
   }, [value]);
+  const phonePlaceHolder = useMemo(() => {
+    const result = phoneNumbersInfo.map((info) => info.placeholder);
+    placeholder
+      ?.split(' ')
+      .slice(0, 3)
+      .forEach((p, i) => {
+        result[i] = p;
+      });
+    return result;
+  }, [placeholder]);
 
   const setPhoneNumber = useCallback(
     (i: number) => (e: React.ChangeEvent<HTMLInputElement>) => {
       const nextPhone = [...phoneValue];
       const currentValue = e.target.value;
-      const phoneNumber = currentValue.replace(/[^0-9]/g, '');
+      const phoneNumber = currentValue.replace(REG_NOT_DIGITS, '');
 
       if (phoneNumber === phoneValue[i]) return;
       nextPhone[i] = phoneNumber;
-      onChange(nextPhone);
+      onChange(nextPhone.join('-'));
     },
     [onChange, phoneValue],
   );
@@ -47,14 +64,16 @@ const PhoneInput: React.FC<Props> = ({ onChange, value }) => {
     <PhoneWrapper>
       {phoneNumbersInfo.map((phoneNumberInfo, i) => {
         return (
-          <React.Fragment key={phoneNumberInfo.key}>
+          <Fragment key={phoneNumberInfo.key}>
             <Input
               type="text"
               width="90px"
-              value={phoneValue[i].slice(0, phoneNumberInfo.max)}
+              value={phoneValue[i]?.slice(0, phoneNumberInfo.max) || ''}
               onChange={setPhoneNumbers[i]}
               maxLength={phoneNumberInfo.max}
               style={{ textAlign: 'center', fontSize: '14px' }}
+              disabled={disabled}
+              placeholder={phonePlaceHolder[i]}
             />
             {i !== phoneNumbersInfo.length - 1 && (
               <Divider
@@ -63,7 +82,7 @@ const PhoneInput: React.FC<Props> = ({ onChange, value }) => {
                 direction="horizontal"
               />
             )}
-          </React.Fragment>
+          </Fragment>
         );
       })}
     </PhoneWrapper>
