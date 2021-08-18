62a3ffbf67f753499292d863442da209afc2e1d3
jonghopark95 <jong951005@gmail.com>
2021-08-19 08:51:37 +0900

    chore: useReducer로 리팩토링한 store 모듈 로직 구현

---
+++ client/src/stores/productListModule.ts
@@ -0,0 +1,60 @@
+import { useReducer } from 'react';
+import { ProductsGetRequestQuery } from '~/lib/api/types';
+
+export type CategoryType = ProductsGetRequestQuery['category'];
+export type OrderType = ProductsGetRequestQuery['order'];
+export type SearchValueType = ProductsGetRequestQuery['search'];
+
+const DEFAULT_PAGE_NUMBER = 1;
+const DEFAULT_PRODUCTS_AMOUNT = 20;
+
+const SET_CATEGORY = 'SET_CATEGORY';
+const SET_ORDER = 'SET_ORDER';
+const SET_SEARCH_VALUE = 'SET_SEARCH_VALUE';
+const SET_NEXT_PAGE = 'SET_NEXT_PAGE';
+
+export const setCategory = (payload: CategoryType) => ({
+  type: SET_CATEGORY,
+  payload,
+});
+export const setOrder = (payload: OrderType) => ({
+  type: SET_ORDER,
+  payload,
+});
+export const setSearchValue = (payload: SearchValueType) => ({
+  type: SET_SEARCH_VALUE,
+  payload,
+});
+export const setNextPage = () => ({ type: SET_NEXT_PAGE });
+
+export const INITIAL_FILTER_STATE: ProductsGetRequestQuery = {
+  order: 'recent',
+  page: DEFAULT_PAGE_NUMBER,
+  limit: DEFAULT_PRODUCTS_AMOUNT,
+};
+
+const filterReducer = (state: ProductsGetRequestQuery, action) => {
+  switch (action.type) {
+    case SET_CATEGORY:
+      return { ...state };
+    case SET_ORDER:
+      return { ...state };
+    case SET_SEARCH_VALUE:
+      return { ...state };
+    case SET_NEXT_PAGE:
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
+
+const productListModule = () => {
+  const [filterState, dispatch] = useReducer(
+    filterReducer,
+    INITIAL_FILTER_STATE,
+  );
+
+  return { filterState, dispatch };
+};
+
+export default productListModule;
