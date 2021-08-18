08b2dc7f924f75e96fe1ceb02efb054f541afb45
jonghopark95 <jong951005@gmail.com>
2021-08-18 22:56:28 +0900

    feat: ProductList에서 사용할 FilterContext 생성
    Co-Authored-By: qudgus21. <47776356+qudgus21@users.noreply.github.com>

--- client/src/pages/ProductList/index.tsx
+++ client/src/pages/ProductList/index.tsx
@@ -1,4 +1,12 @@
-import React, { FC } from 'react';
+import React, { FC, createContext } from 'react';
+import CategoryFilter from '~/components/CategoryFilter';
+import OrderFilter from '~/components/OrderFilter';
+import productModule, {
+  CategoryType,
+  DEFAULT_FILTER,
+  OrderType,
+  SearchValueType,
+} from '~/stores/productModule';
 
 import {
   ProductListWrapper,
@@ -7,21 +15,36 @@ import {
   VerticalDivider,
 } from './index.style';
 
+export const FilterContext = createContext({
+  ...DEFAULT_FILTER,
+  setCategory: (_: CategoryType) => {},
+  setOrder: (_: OrderType) => {},
+  setSearchValue: (_: SearchValueType) => {},
+  setPage: () => {},
+});
+
 const ProductList: FC = () => {
+  const { filter, setCategory, setOrder, setSearchValue, setPage } =
+    productModule();
+
   return (
-    <ProductListWrapper>
-      <LeftSection>
-        <div>카테고리 컴포넌트</div>
-        <div>순서 컴포넌트</div>
-      </LeftSection>
-      <VerticalDivider />
-      <RightSection>
-        <div>카테고리 현재 선택</div>
-        <div>검색</div>
-        <div>상품리스트</div>
-        <div>스크롤</div>
-      </RightSection>
-    </ProductListWrapper>
+    <FilterContext.Provider
+      value={{ ...filter, setCategory, setOrder, setSearchValue, setPage }}
+    >
+      <ProductListWrapper>
+        <LeftSection>
+          <CategoryFilter />
+          <OrderFilter />
+        </LeftSection>
+        <VerticalDivider />
+        <RightSection>
+          <button type="button">카테고리 현재 선택</button>
+          <div>검색</div>
+          <div>상품리스트</div>
+          <div>스크롤</div>
+        </RightSection>
+      </ProductListWrapper>
+    </FilterContext.Provider>
   );
 };
 
