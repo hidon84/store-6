88e0d316dc86937b7db8333c8e2bdd2498023ff2
gomjellie <gomjellie@gmail.com>
2021-08-19 19:31:20 +0900

    [#64]feat: 회원가입방식 선택 페이지 스타일링

--- client/src/pages/SignUp/SignUpTypes.tsx
+++ client/src/pages/SignUp/SignUpTypes.tsx
@@ -1,21 +1,43 @@
 import { FC } from 'react';
 import styled from 'styled-components';
-import { titleSVG } from '~/assets';
+import {
+  signUpWithFacebookSVG,
+  signUpWithGoogleSVG,
+  signUpWithOwnSVG,
+} from '~/assets';
 import Space from '~/components/Space';
+import { HeaderTitle } from './index.fc';
 
 const SignUpTypesWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   width: 400px;
   height: 100%;
   justify-content: center;
   align-items: center;
 `;
 
+const SignUpTypeButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 48px;
+`;
+
+const SignUpTypeButton = styled.img`
+  cursor: pointer;
+`;
+
 const SignUpTypes: FC = () => {
   return (
     <SignUpTypesWrapper>
-      <img src={titleSVG} alt="배민문방구" />
-      <Space aria-hidden />
+      <HeaderTitle />
+      <SignUpTypeButtons>
+        <SignUpTypeButton src={signUpWithOwnSVG} alt="own" />
+        <SignUpTypeButton src={signUpWithFacebookSVG} alt="facebook" />
+        <SignUpTypeButton src={signUpWithGoogleSVG} alt="google" />
+        <Space height="120px" />
+      </SignUpTypeButtons>
     </SignUpTypesWrapper>
   );
 };
