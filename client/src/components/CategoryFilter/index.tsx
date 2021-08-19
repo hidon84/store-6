27ca5e4c171acd54adc07e6d26e547d8636f3cf8
qudgus21 <qudgus54321@naver.com>
2021-08-19 15:50:05 +0900

    [#57]feat: 카테고리 컴포넌트 추가

--- client/src/components/CategoryFilter/index.tsx
+++ client/src/components/CategoryFilter/index.tsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { FilterContext } from '~/pages/ProductList';
+import styled from 'styled-components';
 
 enum CategoryType {
   Book,
@@ -11,17 +12,73 @@ enum CategoryType {
   Ulgiro,
   Collaboration,
   Gift,
-}
+} 
+
+import {
+  BigBookSVG,
+  SmallPencilSVG,
+  SmallBaedalSVG,
+  SmallColabSVG,
+  SmallGiftSVG,
+  Hat2SVG,
+  SmallHouseSVG,
+  SmallKKSVG,
+  SmallTreeSVG,
+  SmallCircleSVG,
+  RefreshSVG
+} from '~/assets';
+
+
+const CategoryFilterWrapper = styled.div`
+
+`
+
+const CategoryHeder = styled.div`
+  font-size: 25px;
+  display: flex;
+  align-items : center;
+  justify-content: space-between;
+  margin-bottom: 33px;
+  img{
+    width: 25px;
+  }
+`
+
+
+const ImageWrapper = styled.div`
+  img{
+    place-self: center;
+    width: 55px;
+  }
+
+  display: grid;
+  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 25px;
+`
+
 
 const CategoryFilter: React.FC = () => {
   const { setCategory, ...state } = useContext(FilterContext);
 
   return (
-    <div>
-      <button type="button" onClick={() => setCategory(CategoryType.Gift)}>
-        sdfsdfsdf
-      </button>
-    </div>
+    <CategoryFilterWrapper>
+      <CategoryHeder>
+        <div>카테고리</div>
+        <img src={RefreshSVG} />
+      </CategoryHeder>
+      <ImageWrapper>
+          <img src={BigBookSVG} />
+          <img src={SmallPencilSVG} />
+          <img src={SmallHouseSVG} />
+          <img src={SmallTreeSVG} />
+          <img src={SmallBaedalSVG} />
+          <img src={SmallKKSVG} />
+          <img src={Hat2SVG} />
+          <img src={SmallGiftSVG} />
+          <img src={SmallColabSVG}/>
+      </ImageWrapper>
+    </CategoryFilterWrapper>
   );
 };
 
