1ecdef3f7a95f43439a8119324059989a50e9854
jonghopark95 <jong951005@gmail.com>
2021-08-19 21:15:20 +0900

    style: 최근 검색어 X에 hover시 filter 처리

--- client/src/components/productList/SearchTerm/index.style.tsx
+++ client/src/components/productList/SearchTerm/index.style.tsx
@@ -12,4 +12,10 @@ export const Term = styled.span`
 
 export const X = styled.img`
   margin-left: 6px;
+
+  transition: filter 0.2s ease;
+  &:hover {
+    filter: invert(64%) sepia(72%) saturate(451%) hue-rotate(127deg)
+      brightness(88%) contrast(85%);
+  }
 `;
