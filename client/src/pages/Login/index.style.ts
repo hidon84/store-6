383dde42ca39d1d6f2ba7fb73139a301b826790c
gomjellie <gomjellie@gmail.com>
2021-08-20 09:58:43 +0900

    [#64] 병현님쪽 스타일링이랑 충돌되는 #app 100vh 제거

--- client/src/pages/Login/index.style.ts
+++ client/src/pages/Login/index.style.ts
@@ -6,7 +6,7 @@ export const StyledLoginPage = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%;
+  height: 100vh;
 `;
 
 export const LeftDoodles = styled.aside`
