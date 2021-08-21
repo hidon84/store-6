7b504bdce508e95fad8728502187d3824e754507
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-21 12:42:50 +0900

    [#92]feat: UserContext 추가

---
+++ client/src/lib/contexts/createNamedContext.ts
@@ -0,0 +1,10 @@
+import { createContext } from 'react';
+
+const createNamedContext = <T = unknown>(name: string, defaultValue: T = null) => {
+  const context = createContext<T>(defaultValue);
+  context.displayName = name;
+
+  return context;
+};
+
+export default createNamedContext;
---
+++ client/src/lib/contexts/userContext.ts
@@ -0,0 +1,12 @@
+import { Dispatch, SetStateAction  } from "react";
+import { UsersGetResponseBody } from "../api/types";
+import createNamedContext from "./createNamedContext";
+
+export interface UserContextState {
+  user: UsersGetResponseBody;
+  setUser: Dispatch<SetStateAction<UsersGetResponseBody>>;
+}
+
+const UserContext = createNamedContext<UserContextState>('UserContext');
+
+export default UserContext;
