0b40bca9e689c3febbcc07703a03984cfab71bcd
gomjellie <gomjellie@gmail.com>
2021-08-21 21:14:28 +0900

    [#99]feat: 상하좌우키로 고슴도치 움직이기

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import {
   Book,
   Baedal,
@@ -16,13 +16,42 @@ import PixelArt from '~/components/main/pixelArts';
 import { MainContainer } from './index.style';
 
 const Main: FC = () => {
+  const [y, setY] = useState(0);
+  const [x, setX] = useState(0);
+
+  const onKeyDown = (event: globalThis.KeyboardEvent) => {
+    switch (event.code) {
+      case 'ArrowUp':
+        setY(Math.max(0, y - 1));
+        break;
+      case 'ArrowDown':
+        setY(Math.min(90, y + 1));
+        break;
+      case 'ArrowLeft':
+        setX(Math.max(0, x - 1));
+        break;
+      case 'ArrowRight':
+        setX(Math.min(90, x + 1));
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      document.body.removeEventListener('keydown', onKeyDown);
+    };
+  }, [onKeyDown]);
   return (
     <MainContainer>
       <PixelArt className="cat" />
       <PixelArt className="sonic" coord={{ left: '15%', top: '30%' }} />
-      <PixelArt className="chicken" coord={{ left: '40%' }} />
+      <PixelArt className="chicken" coord={{ left: `${x}%`, top: `${y}%` }} />
       <PixelArt className="flower" coord={{ right: '10%' }} />
-      <PixelArt className="flower" coord={{ bottom: '20%' }} />
+      <PixelArt className="ladybug" coord={{ bottom: '20%' }} />
       <PixelArt className="hedgehog" coord={{ bottom: '20%', right: '40%' }} />
       <Book />
       <Baedal />
