883cf1af9a40b82de07af8a1236806060b186eeb
qudgus21 <qudgus54321@naver.com>
2021-08-21 13:54:22 +0900

    [#88]feat: 장바구니 삭제 api type 설정

--- client/src/lib/api/cart.ts
+++ client/src/lib/api/cart.ts
@@ -1,11 +1,17 @@
 import request from './request';
-import { CartGetResponseBody } from './types/cart';
+import { CartGetResponseBody} from './types/cart';
 
 export const cartBaseUrl = '/api/cart';
 
 export const cartUrl = {
   cart: cartBaseUrl,
+  cartDetail: (id: number) => `${cartBaseUrl}/${id}`,
+
 };
 
 export const getCartItems = () =>
   request<CartGetResponseBody[]>('GET', cartUrl.cart);
+
+
+export const deleteCartItem = (id: number) =>
+request('DELETE', cartUrl.cartDetail(id));
