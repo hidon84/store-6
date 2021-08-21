52533859d576f323ec611784910c839da6f2c06b
gomjellie <gomjellie@gmail.com>
2021-08-21 17:43:49 +0900

    [#99]feat: Add Pixel arts: cat, chicken, flower, sonic

---
+++ client/src/components/main/pixelArts/flower/index.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import './flower.css';
+
+const Flower: FC = () => {
+  return <div className="flower"> </div>;
+};
+
+export default Flower;
--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -12,11 +12,19 @@ import {
   Stain,
   Logo,
 } from '~/components/main/IconButtons';
+import Cat from '~/components/main/pixelArts/cat';
+import Sonic from '~/components/main/pixelArts/sonic';
+import Chicken from '~/components/main/pixelArts/chicken';
+import Flower from '~/components/main/pixelArts/flower';
 import { MainContainer } from './index.style';
 
 const Main: FC = () => {
   return (
     <MainContainer>
+      <Cat />
+      <Sonic />
+      <Chicken />
+      <Flower />
       <Book />
       <Baedal />
       <Hat />
