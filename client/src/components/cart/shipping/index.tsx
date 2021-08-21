41354811b8d024fa4df00d746231e93a847bfa02
qudgus21 <qudgus54321@naver.com>
2021-08-21 17:48:25 +0900

    [#104]feat: shipping 컴포넌트 파일 생성

--- client/src/components/cart/shipping/index.tsx
+++ client/src/components/cart/shipping/index.tsx
@@ -1,7 +1,58 @@
 import React, { FC, useState } from 'react';
+import styled from 'styled-components';
+import Button from '~/components/common/Button';
+import Divider from '~/components/common/Divider';
+import { alert } from '~/utils/modal';
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  height: 80px;
+  align-items: center;
+  justify-content: flex-end;
+  margin-right: 150px;
+`
+
+const ShipHeader = styled.div`
+  height: 50px;
+  display : flex;
+  align-items: center;
+  font-size: 15px;
+
+  div {
+    width: 100px;
+    margin-left: 170px;
+  }
+
+`
+
 
 const Shipping: FC = () => {
-  return <div>배송지관리 컴포넌트</div>;
+
+  const [shipItems, setShipItems] = useState([]);
+
+  const handleNewBtnClick = () => { 
+      alert('기다려')
+  }
+
+
+  return (
+    <div>
+      <ButtonWrapper>
+        <Button size="lg" onClick={handleNewBtnClick}>
+            신규주소 등록
+        </Button>
+      </ButtonWrapper>
+      <ShipHeader>
+        <div>수령인 정보</div>
+        <div>주소지 정보</div>
+      </ShipHeader>
+      <Divider width="950px" direction="horizontal" />
+      <div>
+        맵돌리는 부분
+      </div>
+      <div>선택주소 사용하기</div>
+    </div>
+  );
 };
 
 export default Shipping;
