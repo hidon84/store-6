c4b8ec9cf1665a1565efb218890d78773abad0da
qudgus21 <qudgus54321@naver.com>
2021-08-21 13:54:43 +0900

    [#88]feat: 장바구니 상품 가져오기 api type 수정

--- client/src/lib/api/types/cart.ts
+++ client/src/lib/api/types/cart.ts
@@ -1,8 +1,17 @@
 export interface CartGetResponseBody {
   idx: number;
-  name: string;
-  thumbnail: string;
-  price: number;
+  product: {
+    idx: number;
+    title: string;
+    thumbnail: string;
+    price: number;
+    description: string;
+    shipSummary: string;
+    shipDetail:string;
+    policy: string;
+    createdAt: string;
+    updatedAt: string;
+  }
   createdAt: string;
   updatedAt: string;
 }
