363e5a3e50c63484fbd68a138e87a09846f2d810
qudgus21 <qudgus54321@naver.com>
2021-08-22 19:48:08 +0900

    [#118]feat: route, controller 틀 생성

--- server/src/api/routes/shipping/index.ts
+++ server/src/api/routes/shipping/index.ts
@@ -1,11 +1,52 @@
 import { Router } from 'express';
 import middlewares from '@/api/middlewares';
-import {} from './shippingController';
+import {handleGetShippingItems, handlePostShippingItem, handlePutShippingItem, handleDeleteShippingItem, handleSelectShippingItem} from './shippingController';
 
 const shippingRouter = Router();
 
 export default (router: Router) => {
-  router.use('/cart', shippingRouter);
+  router.use('/shipping', shippingRouter);
+    
+  shippingRouter.get(
+    '/',
+    middlewares.isAuth,
+    middlewares.isAccessToken,
+    middlewares.attachCurrentUser,
+    handleGetShippingItems,
+  );
 
+  shippingRouter.post(
+    '/',
+    middlewares.isAuth,
+    middlewares.isAccessToken,
+    middlewares.attachCurrentUser,
+    handlePostShippingItem,
+  );
+
+  shippingRouter.put(
+    '/:id',
+    middlewares.isAuth,
+    middlewares.isAccessToken,
+    middlewares.attachCurrentUser,
+    handlePutShippingItem,
+  );
+   
+    
+  shippingRouter.delete(
+    '/:id',
+    middlewares.isAuth,
+    middlewares.isAccessToken,
+    middlewares.attachCurrentUser,
+    handleDeleteShippingItem,
+  );
+    
+  shippingRouter.delete(
+    '/:id/select',
+    middlewares.isAuth,
+    middlewares.isAccessToken,
+    middlewares.attachCurrentUser,
+    handleSelectShippingItem,
+  );
+  
   return router;
 };
--- server/src/api/routes/shipping/shippingController.ts
+++ server/src/api/routes/shipping/shippingController.ts
@@ -1,3 +1,78 @@
 import { NextFunction, Request, Response } from 'express';
 import Container from 'typedi';
 import ShippingService from '@/service/shipping';
+
+
+export const handleGetShippingItems = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+
+  
+      return res.status(200).json();
+    } catch (e) {
+      return next(e);
+    }
+};
+
+
+export const handlePostShippingItem = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+
+  
+      return res.status(200).json();
+    } catch (e) {
+      return next(e);
+    }
+};
+
+
+export const handlePutShippingItem = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+
+  
+      return res.status(200).json();
+    } catch (e) {
+      return next(e);
+    }
+};
+
+
+export const handleDeleteShippingItem = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+
+  
+      return res.status(200).json();
+    } catch (e) {
+      return next(e);
+    }
+};
+
+
+export const handleSelectShippingItem = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    try {
+
+  
+      return res.status(200).json();
+    } catch (e) {
+      return next(e);
+    }
+};
