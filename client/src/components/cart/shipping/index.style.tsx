db959ee50148befd971580e6483667a5982ba48c
qudgus21 <qudgus54321@naver.com>
2021-08-22 16:45:08 +0900

    [#104]feat: 배송지 컴포넌트 스타일링

---
+++ client/src/components/cart/shipping/index.style.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  height: 80px;
+  align-items: center;
+  justify-content: flex-end;
+  margin-right: 150px;
+  &:last-child {
+    margin-top: 40px;
+  }
+`;
+
+export const ShipHeader = styled.div`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  font-size: 15px;
+  div {
+    margin-left: 170px;
+  }
+`;
