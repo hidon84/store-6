d19e8dffa9c1a78e0af427b305d1b79204efbe6b
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-23 22:19:43 +0900

    [#127]feat: ProductItemWrapper 컴포넌트 추가

---
+++ client/src/components/product/ProductItemWrapper/index.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ProductItemWrapper = styled.div`
+  position: relative;
+  height: 300px;
+  width: 230px;
+  cursor: pointer;
+`;
+
+export default ProductItemWrapper;
