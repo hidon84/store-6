308dc3b6d247de4733427b5cb648bf47500a643d
jonghopark95 <jong951005@gmail.com>
2021-08-23 19:14:48 +0900

    [#126] chore: fetchModule에 forcedDelayTime 추가

--- client/src/stores/fetchModule.ts
+++ client/src/stores/fetchModule.ts
@@ -1,46 +1,52 @@
 import { useReducer } from 'react';
 
 // Interface
-export interface FetchState {
+export interface FetchModuleState {
   state: string;
+  forcedDelayTime: number;
 }
 
-export interface FetchAction {
+export interface FetchModuleAction {
   type: string;
 }
 
 // Action
 const START_FETCH = 'START_FETCH';
 const FINISH_FETCH = 'FINISH_FETCH';
+const INIT_FETCH = 'INIT_FETCH';
 
 // Action Creator
 export const startFetch = () => ({ type: START_FETCH });
 export const finishFetch = () => ({ type: FINISH_FETCH });
+export const initFetch = () => ({ type: INIT_FETCH });
 
 // State
 const INITIAL_FETCH_STATE = {
-  state: START_FETCH,
+  state: 'INIT_FETCH',
+  forcedDelayTime: 300,
 };
 
 // Reducer
-const fetchReducer = (state: FetchState, action: FetchAction): FetchState => {
+const fetchReducer = (
+  fetchModuleState: FetchModuleState,
+  action: FetchModuleAction,
+): FetchModuleState => {
   switch (action.type) {
     case START_FETCH:
-      return { state: 'START_FETCH' };
+      return { ...fetchModuleState, state: 'START_FETCH' };
     case FINISH_FETCH:
-      return { state: 'FINISH_FETCH' };
+      return { ...fetchModuleState, state: 'FINISH_FETCH' };
+    case INIT_FETCH:
+      return { ...fetchModuleState, state: 'INIT_FETCH' };
     default:
-      return { ...state };
+      return { ...fetchModuleState };
   }
 };
 
 const fetchModule = () => {
-  const [fetchState, fetchDispatch] = useReducer(
-    fetchReducer,
-    INITIAL_FETCH_STATE,
-  );
+  const [state, dispatch] = useReducer(fetchReducer, INITIAL_FETCH_STATE);
 
-  return { fetchState, fetchDispatch };
+  return { state, dispatch };
 };
 
 export default fetchModule;
