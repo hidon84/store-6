e43b2535c78c5160eed13f2f466400c1585c9c90
jonghopark95 <jong951005@gmail.com>
2021-08-23 19:15:31 +0900

    [#126] chore: ProductItemContainer 스타일 코드 수정

--- client/src/components/productList/ProductItemContainer/index.style.ts
+++ client/src/components/productList/ProductItemContainer/index.style.ts
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 export const ProductItemContainerWrapper = styled.div`
   position: relative;
   width: 100%;
-  height: 100%;
+  height: fit-content;
+  margin-bottom: 50px;
 `;
 
-export const ItemList = styled.ul<{ isFetching: boolean }>`
+export const ItemList = styled.ul<{
+  isFetching: boolean;
+  delayedTime: number;
+}>`
   opacity: ${({ isFetching }) => (isFetching ? 0 : 1)};
-  transition: opacity 0.5s ease-in-out;
+  transition: opacity ${({ delayedTime }) => delayedTime}s ease-in;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 31px;
@@ -17,5 +21,5 @@ export const ItemList = styled.ul<{ isFetching: boolean }>`
 
 export const ListFooter = styled.div`
   width: 100%;
-  height: 20px;
+  height: 40px;
 `;
