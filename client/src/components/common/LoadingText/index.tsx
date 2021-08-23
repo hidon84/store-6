603c0af1e122f3b5ecb490b1461c32d725edeea1
jonghopark95 <jong951005@gmail.com>
2021-08-23 23:49:45 +0900

    [#126] fix: getRandomIndex 유틸 함수로 분리

--- client/src/components/common/LoadingText/index.tsx
+++ client/src/components/common/LoadingText/index.tsx
@@ -1,21 +1,18 @@
 import { FC, useEffect, useState } from 'react';
+import { getRandomIndex } from '~/utils/getRandomIndex';
 
 import { LoadingTextWrapper, Text, TextArea } from './index.style';
 import { PHRASES } from './phrases';
 
-const getRandomIdx = (max: number = PHRASES.length): number => {
-  return Math.floor(Math.random() * max);
-};
-
 const LoadingText: FC<{ isFetching: boolean }> = ({ isFetching }) => {
-  const [phrase, setPhrase] = useState(PHRASES[getRandomIdx()]);
+  const [phrase, setPhrase] = useState(PHRASES[getRandomIndex(PHRASES.length)]);
 
   /**
    * Fetching이 끝날 경우, 다음 Fetching에 대비해 phrase를 미리 만들어 놓습니다.
    */
   useEffect(() => {
     if (!isFetching) {
-      const idx = getRandomIdx();
+      const idx = getRandomIndex(PHRASES.length);
       setPhrase(PHRASES[idx]);
     }
   }, [isFetching]);
