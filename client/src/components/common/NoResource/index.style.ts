e73b4395f4e4f5be898bb782724a909a1ccd683c
jonghopark95 <jong951005@gmail.com>
2021-08-24 14:15:41 +0900

    [#150] feat: NoResource 컴포넌트 개발

---
+++ client/src/components/common/NoResource/index.style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { NoResourceSVG } from '~/assets';
+
+export const NoResourceWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: fit-content;
+`;
+
+export const NoResourceImage = styled.img.attrs({
+  src: NoResourceSVG,
+})`
+  width: 200px;
+  height: 200px;
+`;
+
+export const NoResourceContent = styled.p`
+  width: 100%;
+  text-align: center;
+  margin-top: 19px;
+  font-size: 30px;
+  color: var(--baemin100);
+`;
