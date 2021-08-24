5d892db31f2d874309cc99d5d01adf5758762203
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-24 09:04:28 +0900

    [#145]feat: ProfileImage 컴포넌트 추가

---
+++ client/src/components/common/ProfileImage/index.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+interface Props {
+  src?: string;
+  size?: string;
+}
+
+const ProfileImage = styled.div<Props>`
+  border-radius: 50%;
+  width: ${({ size }) => size ?? '100%'};
+  height: ${({ size }) => size ?? '100%'};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-image: ${({ src }) => (src ? `url(${src})` : 'none')};
+  transition: ease-in-out 750ms;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  cursor: pointer;
+`;
+
+export default ProfileImage;
