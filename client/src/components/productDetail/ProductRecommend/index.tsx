c90c92cf51d7daa9271e3b4243068b95824e4125
qudgus21 <qudgus54321@naver.com>
2021-08-25 22:56:13 +0900

    [#174]refactor: style 분리

--- client/src/components/productDetail/ProductRecommend/index.tsx
+++ client/src/components/productDetail/ProductRecommend/index.tsx
@@ -2,32 +2,8 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
 import React, { FC } from 'react';
-import styled from 'styled-components';
 import { useHistory } from '~/core/Router';
-
-const ProductRecommendContainerWrapper = styled.div`
-  width: 300px;
-  margin-top: 50px;
-`;
-
-const ImageWrapper = styled.div`
-  display: flex;
-  margin-top: 15px;
-  img {
-    width: 70px;
-    height: 91.3px;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))
-      drop-shadow(2px 2px 12px rgba(0, 0, 0, 0.15));
-    border-radius: 10px;
-    margin-right: 15px;
-    cursor: pointer;
-
-    &:hover {
-      transition: transform 300ms;
-      transform: scale(1.1);
-    }
-  }
-`;
+import { ImageWrapper, ProductRecommendContainerWrapper } from './index.style';
 
 interface Props {
   products: {
