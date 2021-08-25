49f2822611233e9228373b89d054f593bb51ec0e
박종호 <jong951005@gmail.com>
2021-08-25 18:32:41 +0900

    [#175] feat: DropdownOpened 상태 및 관리로직 추가

--- client/src/components/base/ProfileIcon/index.tsx
+++ client/src/components/base/ProfileIcon/index.tsx
@@ -1,10 +1,12 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
+
 import ProfileImage from '~/components/common/ProfileImage';
 import { Link } from '~/core/Router';
-import { UsersGetResponseBody, ErrorResponse } from '~/lib/api/types';
-import { UserInteractDropdown, InteractSpan } from './index.style';
 import * as authAPI from '~/lib/api/auth';
+import { UsersGetResponseBody, ErrorResponse } from '~/lib/api/types';
 import { alert } from '~/utils/modal';
+
+import { UserInteractDropdown, InteractSpan } from './index.style';
 
 interface IProps {
   user: UsersGetResponseBody;
@@ -12,6 +14,21 @@ interface IProps {
 }
 
 const ProfileIcon: FC<IProps> = ({ user, pathname }) => {
+  const [isDropdownOpened, setIsDropdownOpened] = useState(false);
+
+  const profileImageRef = useRef<HTMLDivElement>();
+  const profileModalRef = useRef<HTMLDivElement>();
+  const handleMouseClick = useCallback(() => setIsDropdownOpened(true), []);
+  const onDocumentClick = useCallback(({ target }) => {
+    if (![profileModalRef.current, profileImageRef.current].includes(target))
+      setIsDropdownOpened(false);
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('click', onDocumentClick);
+    return () => document.addEventListener('click', onDocumentClick);
+  }, []);
+
   const requestLogout = () => {
     authAPI
       .logout()
@@ -23,8 +40,16 @@ const ProfileIcon: FC<IProps> = ({ user, pathname }) => {
 
   return (
     <>
-      <ProfileImage image={user.profile} size="30px" />
-      <UserInteractDropdown>
+      <ProfileImage
+        image={user.profile}
+        size="30px"
+        onClick={handleMouseClick}
+        ref={profileImageRef}
+      />
+      <UserInteractDropdown
+        isDropdownOpened={isDropdownOpened}
+        ref={profileModalRef}
+      >
         <Link to="/me">
           <InteractSpan>마이페이지</InteractSpan>
         </Link>
