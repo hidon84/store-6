c6c89752c4885b9c592fd91978bad26b5d597c51
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-26 23:40:53 +0900

    [#185]feat: 이미지 확대 컴포넌트 추가

---
+++ client/src/components/productDetail/ImageMagnifier/index.style.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+interface IStyledMagnifierProps {
+  imageSrc: string;
+  width: number;
+  height: number;
+  show: boolean;
+}
+
+export const TargetImage = styled.img.attrs({
+  width: '100%',
+})``;
+
+export const StyledImageMagnifier = styled.div`
+  position: relative;
+  width: 400px;
+`;
+
+export const StyledMagnifier = styled.div<IStyledMagnifierProps>`
+  position: absolute;
+  opacity: ${({ show }) => (show ? '1' : '0')};
+  border-radius: 50%;
+  box-shadow: 0 0 0 3px rgba(255, 255, 255, 0.85),
+    0 0 3px 3px rgba(0, 0, 0, 0.25);
+
+  background: ${({ imageSrc }) => `url(${imageSrc})`} no-repeat;
+
+  ${({ width, height }) => `
+    width: ${width}px;
+    height: ${height}px;
+  `}
+`;
