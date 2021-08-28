9db1e60f411723fecb549b3b0195af4af479e165
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-28 14:48:03 +0900

    [#240]refactor: useInputValidator에서 onInput을 반환하도록 수정

--- client/src/lib/hooks/useInputValidator.ts
+++ client/src/lib/hooks/useInputValidator.ts
@@ -8,15 +8,19 @@ const useInputValidator = (
   const [input, setInput] = useState(initialValue);
   const [warning, setWarning] = useState(' ');
 
-  const onInput = (userInput: string) => {
-    setInput(userInput);
-    setWarning(validator(userInput));
-  };
+  const onInput = useCallback(
+    (userInput: string) => {
+      setInput(userInput);
+      setWarning(validator(userInput));
+    },
+    [setInput, setWarning, validator, ...deps],
+  );
 
   const handleInput = useCallback((ev: React.ChangeEvent<HTMLInputElement>) => {
     onInput(ev.target.value);
   }, deps);
-  return [input, warning, handleInput] as const;
+
+  return [input, warning, handleInput, onInput] as const;
 };
 
 export default useInputValidator;
