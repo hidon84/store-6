e525aa679297d36dd5fa891be1c224eda14fb06c
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-28 14:40:08 +0900

    [#240]feat: PhoneInput 공통 컴포넌트 추가

---
+++ client/src/components/common/PhoneInput/index.style.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import { phoneUnderLineSVG } from '~/assets';
+
+export const PhoneWrapper = styled.div<{ fontSize }>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  font-size: ${({ fontSize }) => fontSize || 'inhert'};
+`;
+
+export const StyledPhoneInput = styled.input`
+  text-align: center;
+  box-sizing: border-box;
+  width: 100px;
+  height: 34px;
+  background: bottom left / contain no-repeat url(${phoneUnderLineSVG});
+  font-size: inherit;
+  line-height: inherit;
+
+  &::placeholder {
+    color: #999;
+  }
+`;
