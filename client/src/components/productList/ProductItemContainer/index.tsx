659461a06ae6df7a9f4b1782a079c77ab10ec963
박종호 <jong951005@gmail.com>
2021-08-30 13:12:44 +0900

    [#165] chore: 상품이 없을 경우 "끝" 안보이도록 수정

--- client/src/components/productList/ProductItemContainer/index.tsx
+++ client/src/components/productList/ProductItemContainer/index.tsx
@@ -52,7 +52,7 @@ const ProductItemContainer: ForwardRefRenderFunction<HTMLDivElement, Props> = (
             />
           ))}
         <S.NoResourceWrapper>
-          {products.length === 0 && fetchState.action !== INIT_FETCH && (
+          {products.length === 0 && fetchState.action !== START_FETCH && (
             <NoResource content={NO_RESOURCE_CONTENT} />
           )}
         </S.NoResourceWrapper>
@@ -62,7 +62,9 @@ const ProductItemContainer: ForwardRefRenderFunction<HTMLDivElement, Props> = (
         {fetchState.action === FINISH_FETCH && !filterState.isLastPage && (
           <S.LoadingText>로딩중</S.LoadingText>
         )}
-        {filterState.isLastPage && <S.LoadingText>끝</S.LoadingText>}
+        {products.length !== 0 && filterState.isLastPage && (
+          <S.LoadingText>끝</S.LoadingText>
+        )}
       </S.ListFooter>
     </S.ProductItemContainerWrapper>
   );
