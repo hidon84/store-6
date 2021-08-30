41fa06ab61e16b07881b4e7c01b1a2b25c825064
박종호 <jong951005@gmail.com>
2021-08-31 07:05:19 +0900

    docs: debounce, cssParser 주석 개선

--- client/src/lib/customStyledComponents/constructWithTag.tsx
+++ client/src/lib/customStyledComponents/constructWithTag.tsx
@@ -14,6 +14,9 @@ export type PropsType = { [key: string]: unknown };
 
 /**
  * Tag를 받아 Component 클로져를 반환합니다.
+ *
+ * 자세한 설명은 아래 링크에 기술하였습니다.
+ * https://github.com/woowa-techcamp-2021/store-6/wiki/custom-styled-components-%EA%B0%9C%EB%B0%9C%EA%B8%B0
  */
 const constructWithTag = (tag: string) => {
   const Tag = `${tag}` as keyof JSX.IntrinsicElements;
--- client/src/lib/customStyledComponents/utils/cssParser.ts
+++ client/src/lib/customStyledComponents/utils/cssParser.ts
@@ -2,7 +2,11 @@ import { PropsType } from '~/lib/customStyledComponents/constructWithTag';
 
 /**
  * Template String으로 둘러쌓인 string은 배열로 들어오게 됩니다.
- * cssParser는 이 배열을 한 줄의 string으로 만듭니다.
+ * 만약 중간에 ${expression} 형태로 둘러쌓인 placeholder를 만나게 된다면,
+ * args에 넣고 그 이후의 정보를 strings의 배열 다음 인덱스에 넣게 됩니다.
+ *
+ * 즉, cssParser는 template string 배열을 순회하며
+ * placeholder가 있을 경우 이를 해석하여 string을 반환하게 됩니다.
  */
 const cssParser = (
   strings: TemplateStringsArray,
