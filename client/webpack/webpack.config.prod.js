232a1e2197e156bef2829b518692964991311194
Yunseo Hwang <yunseo.dev@gmail.com>
2021-08-30 17:01:09 +0900

    chore: production mode webpack 설정파일에서 devServer 삭제

--- client/webpack/webpack.config.prod.js
+++ client/webpack/webpack.config.prod.js
@@ -63,10 +63,4 @@ module.exports = {
       },
     },
   },
-  devServer: {
-    contentBase: path.join(__dirname, '../dist'),
-    compress: true,
-    port: 8080,
-    historyApiFallback: true,
-  },
 }
